ed9e0e17ca21fa22b68fd3dd6534d68b2af0540d
Jack T <jackmt9@gmail.com>
2020-01-15 22:52:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,10 +42,7 @@ def monopoly_with_third_tier
         :reading_railroad=>{}, 
         :pennsylvania_railroad=>{}, 
         :b_and_o_railroad=>{}, 
-        :shortline_railroad=>
-        {
-          "mortgage_value"=>"$800"
-        }
+        :shortline_railroad=>{}
       }
     }
   }
