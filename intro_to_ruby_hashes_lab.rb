71d7512f31f456bb76845f2faff108bd04aa028b
Jack T <jackmt9@gmail.com>
2020-01-15 22:49:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_second_tier
     }
   }
 end
-binding.pry
+
 def monopoly_with_third_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
@@ -49,6 +49,7 @@ def monopoly_with_third_tier
   }
 end
 
+binding.pry
 def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
