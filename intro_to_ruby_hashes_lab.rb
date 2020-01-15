fdb6bd7615537f65753cf952ddc8db4732d2cdb1
Jack T <jackmt9@gmail.com>
2020-01-15 22:49:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,4 @@
+require "pry"
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
@@ -20,7 +21,7 @@ def monopoly_with_second_tier
     }
   }
 end
-
+binding.pry
 def monopoly_with_third_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
